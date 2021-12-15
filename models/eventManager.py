5be0f4858cfd7d641d22c67b6b2f63a78052b74c
Bot-Steven <83646314+Bot-Steven@users.noreply.github.com>
2021-12-15 14:20:16 +0100

    Se ha realizado los modelos client y eventManager. Solo falta revisar las relaciones

---
+++ models/eventManager.py
@@ -0,0 +1,22 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+from odoo import models, fields, api
+
+class EventManager(models.Model):
+    
+    _name = 'kedamos.eventManager'
+    _inherit = 'res.users'
+        
+    managerCategory = fields.Selection(('ocio','OCIO'),
+                                       ('deportes','DEPORTES'),
+                                       ('fiesta','FIESTA'),
+                                       ('cultura','CULTURA'),
+                                       ('excursiones','EXCURSIONES'),
+                                       ('videojuegos','VIDEOJUEGOS'),
+                                       ('juegos_de_mesa','JUEGOS_DE_MESA'),
+                                       ('musica','MUSICA'),
+                                       ('otros','OTROS'))
+                                       
+    myRevisions = fields.One2many('kedamos.revise', 'user')
---
+++ models/user.py
@@ -0,0 +1,20 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+from odoo import models, fields, api
+
+class User(models.Model):
+    
+    _name = 'res.users'
+    _inherit = 'res.users'
+    
+    fullName = fields.Char()
+    status = fields.Selection(('user', 'USER'),('admin','ADMIN'))
+    lastPasswordChange = fields.DateTime()
+    email = fields.Char()
+    username = fields.Char()
+    password = fields.Char()
+    privilege = fields.Selection(('enabled','ENABLED'),('disabled','DISABLED'))
+    
+    
