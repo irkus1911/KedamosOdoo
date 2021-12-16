00976847c3be6eb459ea119c1fbcbc4c52f9011c
UnaiUrtiaga <penemax572@gmail.com>
2021-12-16 10:00:13 +0100

    Imports introducidos en los modelos que faltaban

--- models/event.py
+++ models/event.py
@@ -2,6 +2,8 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+from odoo import models, fields
+
 class Event(models.Model):
     _name= 'kedamos.event'
     
--- models/personal_resource.py
+++ models/personal_resource.py
@@ -2,6 +2,8 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+from odoo import models, fields
+
 class personal_resource(models.Model):
     _name='kedamos.personal_resource'
     
