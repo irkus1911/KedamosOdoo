1bd7bf5b2fb4e5cb4def2bed1f421095b87466f6
irkus1911 <83645063+irkus1911@users.noreply.github.com>
2021-12-16 12:12:58 +0100

    Añadidos los modelos

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import client
+from . import comment
+from . import event
+from . import event
+from . import eventManager
+from. import models
+from . import personal_resource
+from . import place
+from . import revise
+from . import user
