0eda83d07a903fd4c5154b774f28b79b33bb1a5e
irkus1911 <83645063+irkus1911@users.noreply.github.com>
2021-12-22 12:30:36 +0100

    removido el import sobreante y espacio dado

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,8 @@
 from . import client
 from . import comment
 from . import event
-from . import event
 from . import eventManager
-from. import models
+from . import models
 from . import personal_resource
 from . import place
 from . import revise
