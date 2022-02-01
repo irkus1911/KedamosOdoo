e50e81fcec988130a9104aa3ad771b61bf769a2a
Bot-Steven <freakermine8@gmail.com>
2022-02-02 00:23:58 +0100

    Se ha añadido el xml de client_view

--- __manifest__.py
+++ __manifest__.py
@@ -26,7 +26,9 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/client_view.xml',
         'views/templates.xml',
+        
     ],
     # only loaded in demonstration mode
     'demo': [
